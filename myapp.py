df1340536d7b970aebcce28790ad900eb64007b2
vekuh19 <primus.vekuh@gmail.com>
2019-06-04 22:31:56 +0000

    added new code

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
+
 print("Hellow World, this is my first program")
 
 This is my new file in here. It was not in the original file.
-print("this is the third line of my code modified")
+print("this is the third line of my code and this code was modifed before the previous modifications that were saved as modified. modified")
